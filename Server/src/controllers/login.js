4b6b00b2aee21530288b89ad59bf99fca949ef37
Elias Martinez <99233887+XliazZz@users.noreply.github.com>
2023-04-23 15:18:07 -0300

    terminando login

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,7 +1,16 @@
-
+const users = require("../utils/users");
 
 const login = (req, res) => {
     const { email, password } = req.query;
+
+    const user = users.find(user => user.email === email && user.password === password);
+
+    if(user){
+        res.status(200).json({access: true})
+    }
+    else{
+        res.status(200).json({access: false})
+    }
 }
 
 module.exports = {
